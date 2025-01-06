4e13e15af6c2595119378d8ca4eedb9ee6e6facb
thphuc2703 <thphucnguyen2703@gmail.com>
2025-01-06 22:43:07 +0700

    add Lab01

---
+++ Lab01/Client/Client.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	kvs "Golang/Lab01/Shared"
+	"fmt"
+	"log"
+	"net/rpc"
+	"strconv"
+	"sync"
+)
+
+func connect() *rpc.Client {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("[C] Dialing error:", err)
+	}
+	return client
+}
+
+func put(key, value string) {
+	client := connect()
+	args := kvs.PutArgs{Key: key, Value: value}
+	reply := kvs.PutReply{}
+	err := client.Call("KV.Put", &args, &reply)
+	if err != nil {
+		log.Fatal("[C] Error: ", err)
+	}
+	errClose := client.Close()
+
+	if errClose != nil {
+		log.Println("[C] Error closing connection:", err)
+	}
+}
+
+func get(key string) string {
+	client := connect()
+	args := kvs.GetArgs{Key: key}
+	reply := kvs.GetReply{}
+	errCall := client.Call("KV.Get", &args, &reply)
+	if errCall != nil {
+		log.Fatal("[C] Error calling service: ", errCall)
+	}
+	errClose := client.Close()
+	if errClose != nil {
+		log.Println("[C] Error closing connection:", errClose)
+	}
+	return reply.Value
+}
+
+func main() {
+	key := "24C11058-NGUYEN_THIEN_PHUC"
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			putValue := strconv.Itoa(n)
+			fmt.Printf("[C] Calling put(\"%s\",\"%s\")...\n",
+				key, putValue)
+			put(key, putValue)
+		}(i)
+	}
+
+	wg.Wait()
+	value := get(key)
+	fmt.Printf("[C] get (%s): %s\n", key, value)
+}
---
+++ Lab01/Shared/types.go
@@ -0,0 +1,24 @@
+package Shared
+
+const (
+	OK       = "OK"
+	ErrNoKey = "ErrNoKey"
+)
+
+type Err string
+
+type PutArgs struct {
+	Key   string
+	Value string
+}
+
+type PutReply struct {
+	Err Err
+}
+type GetArgs struct {
+	Key string
+}
+type GetReply struct {
+	Err   Err
+	Value string
+}
